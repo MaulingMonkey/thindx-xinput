6c351738890c84277f88908d3cfc6cbea858cc04
MaulingMonkey <git@maulingmonkey.com>
2024-01-17 16:09:04 -0800

    simplify paths: eliminate "headers" nonsense

--- src/_lib.rs
+++ src/_lib.rs
@@ -25,7 +25,62 @@
 
 pub mod error; #[doc(no_inline)] pub use error::Error;
 
-#[path="headers/xinput.h/xinput.rs"]            mod xinput_h;   pub use xinput_h::*;
+macro_rules! check_success { ( $err:expr ) => { $crate::check_error_success(&_THINDX_FN_CONTEXT, $err) } }
+
+mods! {
+    inl mod dll {
+        inl mod imports;
+    }
+
+    inl mod enumerations {
+        inl mod battery_devtype;
+        inl mod battery_level;
+        inl mod battery_type;
+        inl mod devsubtype;
+        inl mod devtype;
+        inl mod user;
+        inl mod vk;
+    }
+
+    inl mod flags {
+        inl mod buttons;
+        inl mod caps;
+        inl mod flag;
+        inl mod keystroke;
+    }
+
+    inl mod functions {
+        inl mod enable_;
+        inl mod get_audio_device_ids_;
+        inl mod get_battery_information_;
+        inl mod get_capabilities_;
+        inl mod get_dsound_audio_device_guids_;
+        inl mod get_keystroke_;
+        inl mod get_state_;
+        inl mod get_state_ex_;
+        inl mod set_state_;
+        #[cfg(doc)] pub mod todo;
+    }
+
+    inl mod structures {
+        inl mod audio_device_ids;
+        inl mod battery_information;
+        inl mod capabilities;
+        inl mod dsound_audio_device_guids;
+        inl mod gamepad;
+        inl mod keystroke;
+        inl mod state;
+        inl mod vibration;
+    }
+}
+
+pub(crate) fn check_error_success(fn_context: &'static crate::error_macros::FnContext, err: u32) -> Result<(), Error> {
+    if err == winresult::ERROR::SUCCESS.to_u32() {
+        Ok(())
+    } else {
+        Err(Error(fn_context, error::Kind::from_u32(err)))
+    }
+}
 
 //#[cfg(doc)] #[doc = include_str!("../doc/changelog.md")] pub mod _changelog {}
 #[cfg(doc)] pub mod _headers;
